0b4d014baf6ded95830caadd801661d28ff1466b
mnpkoerner <mnpkoerner@gmail.com>
2020-12-11 18:17:39 -0600

    add employee to array, add employee to table, clear inputs, style

--- client.js
+++ client.js
@@ -1,6 +1,48 @@
 $(document).ready(readyNow)
 function readyNow() {
-    console.log('jq ready')
+    $('#submitButton').on('click', runAndRender)
+    $
+}
+let employees = [];
+
+
+function runAndRender() {
+    let newEmployee = addToArray();
+    emptyInputs();
+    addToTable(newEmployee);
 }
 
-console.log('js ready')
+function addToArray() {
+    let newEmployee = {
+        firstName: $('#employeeFirstName').val(),
+        lastName: $('#employeeLastName').val(),
+        employeeID: $('#employeeID').val(),
+        title: $('#employeeTitle').val(),
+        annualSalary: Number($('#employeeAnnualSalary').val()),
+        monthlySalary: Number($('#employeeAnnualSalary').val()) / 12,
+    }
+    employees.push(newEmployee);
+    console.log(employees)
+    return newEmployee;
+}
+
+function emptyInputs() {
+    $('#employeeFirstName').val('');
+    $('#employeeLastName').val('');
+    $('#employeeID').val('');
+    $('#employeeTitle').val('');
+    $('#employeeAnnualSalary').val('');
+    $('#employeeAnnualSalary').val('');
+}
+
+function addToTable(employee) {
+    let newRow = $(`<tr class="employeeInTable">
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.employeeID}</td>
+        <td>${employee.title}</td>
+        <td>${employee.annualSalary}</td>
+        <td><button class="deleteButton">DELETE</button></td>
+    `);
+    $('#addEmployeeTable').append(newRow);
+}
